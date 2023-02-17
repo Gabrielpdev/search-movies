d08e31febd5de7f182c2be91313e00d83be84b85
Gabriel Pereira <gabriel9938@gmail.com>
2023-02-17 16:37:59 -0300

    chore: initial settings

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import {
+  FlexBox,
+  FlexBoxAlignItems,
+  FlexBoxDirection,
+  FlexBoxJustifyContent,
+  Link,
+  LinkDesign,
+  ShellBar
+} from '@ui5/webcomponents-react'
+import './styles.scss'
+
+export function Header() {
+  return <FlexBox>HEADER</FlexBox>
+}
